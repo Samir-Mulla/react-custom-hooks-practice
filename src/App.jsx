39eb2dbd89e771b7ec1d3f53402701db14007eb9
Samir Mulla <samirmulla710@gmail.com>
2024-03-10 22:12:44 +0530

    modified : src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import useDimensions from "./hooks/useDimensions";
 
 function App() {
+  const dimensions = useDimensions();
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    <h1 className="text-3xl text-center mt-10">Browser  Window size identifier :</h1>
+    <ul className="h-screen flex justify-center items-center gap-96 text-2xl font-bold italic underline underline-offset-4">
+      <li>width : {dimensions.width}</li>
+      <li>height : {dimensions.height}</li>
+    </ul>
     </>
   );
 }
