656bd0c6979c60f894bcf31ca2b8373a0bace6cf
Samir Mulla <samirmulla710@gmail.com>
2024-03-10 22:34:31 +0530

    modified : src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import useDimensions from "./hooks/useDimensions";
 function App() {
   const dimensions = useDimensions();
   return (
-    <>
-    <h1 className="text-3xl text-center mt-10">Browser  Window size identifier :</h1>
-    <ul className="h-screen flex justify-center items-center gap-96 text-2xl font-bold italic underline underline-offset-4">
+    <React.Fragment>
+    <div className="bg-yellow-100">
+    <h1 className="text-3xl text-center pt-6 font-bold cursor-pointer underline underline-offset-[10px] decoration-wavy">Browser  Window size identifier :</h1> 
+    <ul className="h-screen flex justify-around items-center text-2xl font-bold italic underline underline-offset-4 ">
       <li>width : {dimensions.width}</li>
       <li>height : {dimensions.height}</li>
     </ul>
-    </>
+    </div>
+    </React.Fragment>
   );
 }
 
